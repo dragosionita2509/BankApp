3f0e512622d1246c7bf30b97408463fb4ebad4ec
Ionita Dragos-Adrian <ionita_dragos25@yahoo.com>
2021-05-24 15:57:38 +0300

    Meniu + operatii CRUD

--- src/BrainBank/Main.java
+++ src/BrainBank/Main.java
@@ -32,7 +32,112 @@ class SortByName implements Comparator<Bank> {
 
 }
 
+
 public class Main {
+
+    private static void menuOutput() {
+        System.out.println("\n\n\nBine ati venit!\n\n");
+        System.out.println(" 1. Afisati din baza de date");
+        System.out.println(" 2. Introduceti in baza de date");
+        System.out.println(" 3. Modificati in baza de date");
+        System.out.println(" 4. Stergeti din baza de date");
+    }
+
+    private static void DatabaseOutput(ArrayList<IndividualClient> clientList, ArrayList<Bank> bankList, ReadFunctions read) {
+        bankList.clear();
+        clientList.clear();
+        read.readBank(bankList);
+        read.readClient(clientList);
+        read.readCard(clientList);
+        read.readCurrentAccount(clientList);
+        read.readSavingsAccount(clientList);
+        System.out.println("\nBanci: \n");
+        System.out.println(bankList);
+        System.out.println("\nlienti, carduri, conturi: \n");
+        System.out.println(clientList);
+    }
+
+
+    private static void InsertMenu(InsertFunctions ins, Scanner scanner) {
+        System.out.println("\n\nCe doriti sa introduceti?\n");
+        int op;
+        System.out.println(" 1. Banca\n");
+        System.out.println(" 2. Client\n");
+        op = scanner.nextInt();
+        scanner.nextLine();
+        if(op == 1)
+        {
+            String title;
+            String address;
+            System.out.println("Introduceti titlul bancii: ");
+            title = scanner.nextLine();
+            System.out.println("Introduceti adresa bancii: ");
+            address = scanner.nextLine();
+            ins.InsertBank(new Bank(title,address));
+        }
+        else if(op==2)
+        {
+            String CNP;
+            String last_name;
+            String first_name;
+            int age;
+            System.out.println("Introduceti CNP");
+            CNP = scanner.nextLine();
+            System.out.println("Introduceti nume: ");
+            last_name = scanner.nextLine();
+            System.out.println("Introduceti prenume: ");
+            first_name = scanner.nextLine();
+            System.out.println("Varsta: ");
+            age = scanner.nextInt();
+            scanner.nextLine();
+            ins.InsertClient(new IndividualClient(CNP,last_name,first_name,age));
+        }
+    }
+
+    private static void UpdateMenu(ArrayList<Bank> bankList, UpdateFunctions update, Scanner scanner) {
+        int op;
+        System.out.println("\n\nCe doriti sa actualizati?");
+        System.out.println("\n 1. Titlul unei banci");
+        op = scanner.nextInt();
+        scanner.nextLine();
+        if(op == 1)
+        {
+            int idx;
+            System.out.println("Care banca?(index)");
+            idx = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Titlul nou: ");
+            String newTitle;
+            newTitle = scanner.nextLine();
+            update.updateBankTitle(bankList.get(idx),newTitle);
+        }
+    }
+
+    private static void DeleteMenu(ArrayList<IndividualClient> clientList, ArrayList<Bank> bankList, DeleteFunctions delete, Scanner scanner) {
+        int op;
+        System.out.println("Ce doriti sa eliminati?");
+        System.out.println(" 1. O banca");
+        System.out.println(" 2. Un client");
+        op = scanner.nextInt();
+        scanner.nextLine();
+        if(op == 1)
+        {
+            int idx;
+            System.out.println("Care banca?(index)");
+            idx = scanner.nextInt();
+            scanner.nextLine();
+            delete.deleteBank(bankList.get(idx));
+        }
+        else if(op == 2)
+        {
+            int idx;
+            System.out.println("Care client?(index)");
+            idx = scanner.nextInt();
+            scanner.nextLine();
+            delete.deleteClient(clientList.get(idx));
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
 
@@ -44,26 +149,27 @@ public class Main {
         ReadFunctions read = new ReadFunctions();
         DeleteFunctions delete = new DeleteFunctions();
         UpdateFunctions update = new UpdateFunctions();
-        IndividualClient client1 = new IndividualClient("5000824355012","Petrache","Andrei-Marius",24);
-        Account acc1 = new CurrentAccount("RO90BACX0000001731067002",500806.0f,"25 FEB 2004","RON");
-        Account acc2 = new SavingsAccount("RO45RZBR0000531921335683",4250.0f,"9 JAN 2005",0.2f,1,"EUR");
-        Card card1 = new Card("4491251699134462","29 APR 2023",215,9955,acc1);
-        Card card2 = new Card("9986112355981106","5 NOV 2021",449,1234,acc2);
-        Bank bank1 = new Bank("Banca smecherilor", "Strada nr 3");
+        boolean ok = true;
+        while (ok) {
+            int option;
+            Scanner scanner = new Scanner(System.in);
+            menuOutput();
+            option = scanner.nextInt();
+            if (option == 1) {
+                DatabaseOutput(clientList, bankList, read);
+            } else if (option == 2) {
+                InsertMenu(ins, scanner);
+            }
+            else if (option == 3) {
+                UpdateMenu(bankList, update, scanner);
+            }
+            else if (option == 4) {
+                DeleteMenu(clientList, bankList, delete, scanner);
+            }
 
-
-        read.readBank(bankList);
-        read.readClient(clientList);
-        read.readCard(clientList);
-        read.readCurrentAccount(clientList);
-        read.readSavingsAccount(clientList);
-
-        update.updateSavingsAccountinterestRate((SavingsAccount) acc2,0.69f);
-
-
-        System.out.println(bankList);
-        System.out.println(clientList);
+        }
 
 
     }
+
 }
